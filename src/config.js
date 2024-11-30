47cef4cf3b76cf5fc12fe918d56cfa38b4237402
Doan Ngoc Cuong <cuongmkmtpgoldfinch@gmail.com>
2024-11-30 21:52:51 +0700

    [REACT_APP_API_URL=http://103.253.20.13:25037 - Override URL API mặc định trong config.js ] TRƯỚC CHẠY ĐƯỢC GIỜ KHÔNG CHẠY ĐƯỢC -- do gửi file update prod.yml cho a Trúc
    [REACT_APP_API_URL=http://103.253.20.13:25037 - Override URL API mặc định trong config.js ] TRƯỚC CHẠY ĐƯỢC GIỜ KHÔNG CHẠY ĐƯỢC -- do gửi file update prod.yml cho a Trúc
    
    TRƯỚC CHẠY ĐƯỢC GIỜ KHÔNG CHẠY ĐƯỢC -- ra là mình có gửi a Trúc 1 file UPDATE của: docker-compose.prod.yml
    
    frontend:
      build:
        args:
          - REACT_APP_API_URL=http://103.253.20.13:25037  # (1) Build-time
      environment:
        - REACT_APP_API_URL=http://103.253.20.13:25037    # (2) Runtime

--- src/config.js
+++ src/config.js
@@ -1,9 +1,26 @@
+// const config = {
+//   development: {
+//     API_BASE_URL: process.env.REACT_APP_API_URL || 'http://localhost:5000'
+//   },
+//   production: {
+//     API_BASE_URL: process.env.REACT_APP_API_URL || 'http://103.253.20.13:5000'
+//   }
+// };
+
+// export default config[process.env.NODE_ENV || 'development']; 
+
+
+// Trong docker-compose.prod.yml có thêm dòng [REACT_APP_API_URL=http://103.253.20.13:25037 
+// - Override URL API mặc định trong config.js ] 
+// TRƯỚC CHẠY ĐƯỢC GIỜ KHÔNG CHẠY ĐƯỢC -- do gửi file update prod.yml cho a Trúc 
+
+
 const config = {
   development: {
-    API_BASE_URL: process.env.REACT_APP_API_URL || 'http://localhost:5000'
+    API_BASE_URL: process.env.REACT_APP_API_URL || 'http://localhost:25037'
   },
   production: {
-    API_BASE_URL: process.env.REACT_APP_API_URL || 'http://103.253.20.13:5000'
+    API_BASE_URL: process.env.REACT_APP_API_URL || 'http://103.253.20.13:25037'
   }
 };
 
